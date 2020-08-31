f26091e7262989087755dc83693db8c24ea49590
ley-288 <67469616+ley-288@users.noreply.github.com>
2020-08-31 17:14:33 +0200

    first commit

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What is your name?\n");                   //String = 'name'. Get string by asking prompted question.
+    printf("Hello, %s\n", name);                                        //Say hello + the now gained String. ie User name. write comma Name to show computer this is the
+}                                                                       //                                                                           string we need here.
